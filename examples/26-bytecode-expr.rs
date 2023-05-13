6f4cb2e7783265deddd94aa4d5eb8c3c6d6d1b42
msakuta <masahiro.sakuta@gmail.com>
2023-05-13 14:18:27 +0900

    Use serialize_size in 26-bytecode-expr

--- examples/26-bytecode-expr.rs
+++ examples/26-bytecode-expr.rs
@@ -12,6 +12,7 @@ use nom::{
   sequence::{delimited, pair},
   IResult,
 };
+use rusty_programmer::{dprintln, parse_args, RunMode};
 
 #[derive(Debug, Clone, Copy)]
 #[repr(u8)]
@@ -68,6 +69,21 @@ impl Instruction {
   }
 }
 
+fn serialize_size(
+  sz: usize,
+  writer: &mut impl Write,
+) -> std::io::Result<()> {
+  writer.write_all(&(sz as u32).to_le_bytes())
+}
+
+fn deserialize_size(
+  reader: &mut impl Read,
+) -> std::io::Result<usize> {
+  let mut buf = [0u8; std::mem::size_of::<u32>()];
+  reader.read_exact(&mut buf)?;
+  Ok(u32::from_le_bytes(buf) as usize)
+}
+
 struct Compiler {
   literals: Vec<f64>,
   instructions: Vec<Instruction>,
@@ -89,6 +105,7 @@ impl Compiler {
     ret as u8
   }
 
+  /// Returns absolute position of inserted value
   fn add_inst(&mut self, op: OpCode, arg0: u8) -> usize {
     let inst = self.instructions.len();
     self.instructions.push(Instruction { op, arg0 });
@@ -104,11 +121,11 @@ impl Compiler {
     inst
   }
 
-  fn writer_literals(
+  fn write_literals(
     &self,
     writer: &mut impl Write,
   ) -> std::io::Result<()> {
-    writer.write_all(&self.literals.len().to_le_bytes())?;
+    serialize_size(self.literals.len(), writer)?;
     for value in &self.literals {
       writer.write_all(&value.to_le_bytes())?;
     }
@@ -119,7 +136,7 @@ impl Compiler {
     &self,
     writer: &mut impl Write,
   ) -> std::io::Result<()> {
-    writer.write_all(&self.instructions.len().to_le_bytes())?;
+    serialize_size(self.instructions.len(), writer)?;
     for instruction in &self.instructions {
       instruction.serialize(writer).unwrap();
     }
@@ -147,11 +164,9 @@ impl Compiler {
         let lhs = self.compile_expr(lhs);
         let rhs = self.compile_expr(rhs);
         self.add_copy_inst(lhs);
-        self.target_stack.push(self.target_stack[lhs as usize]);
         self.add_copy_inst(rhs);
         self.target_stack.pop();
         self.add_inst(OpCode::Add, 0);
-        self.target_stack.push(usize::MAX);
         self.target_stack.len() - 1
       }
     }
@@ -161,6 +176,7 @@ impl Compiler {
     &self,
     writer: &mut impl Write,
   ) -> std::io::Result<()> {
+    use OpCode::*;
     writeln!(writer, "Literals [{}]", self.literals.len())?;
     for (i, con) in self.literals.iter().enumerate() {
       writeln!(writer, "  [{i}] {}", *con)?;
@@ -171,38 +187,46 @@ impl Compiler {
       "Instructions [{}]",
       self.instructions.len()
     )?;
-    for inst in &self.instructions {
+    for (i, inst) in self.instructions.iter().enumerate() {
       match inst.op {
-        OpCode::LoadLiteral => {
-          writeln!(writer, "  LoadLiteral {}", inst.arg0)?
-        }
-        OpCode::Copy => {
-          writeln!(writer, "  Copy {}", inst.arg0)?
-        }
-        OpCode::Add => writeln!(writer, "  Add")?,
+        LoadLiteral => writeln!(
+          writer,
+          "  [{i}] {:?} {} ({:?})",
+          inst.op, inst.arg0, self.literals[inst.arg0 as usize]
+        )?,
+        OpCode::Copy => writeln!(
+          writer,
+          "  [{i}] {:?} {}",
+          inst.op, inst.arg0
+        )?,
+        OpCode::Add => writeln!(writer, "  [{i}] Add")?,
       }
     }
     Ok(())
   }
 }
 
-fn write_program(file: &str) -> std::io::Result<()> {
+fn write_program(
+  source: &str,
+  writer: &mut impl Write,
+  out_file: &str,
+  disasm: bool,
+) -> std::io::Result<()> {
   let mut compiler = Compiler::new();
-  let input = "10 + (100 + 1)";
-  let (_, ex) = expr(input).map_err(|e| {
-    std::io::Error::new(std::io::ErrorKind::Other, e)
+  let (_, ex) = expr(source).map_err(|e| {
+    std::io::Error::new(std::io::ErrorKind::Other, e.to_owned())
   })?;
 
   compiler.compile_expr(&ex);
 
-  compiler.disasm(&mut std::io::stdout())?;
+  if disasm {
+    compiler.disasm(&mut std::io::stdout())?;
+  }
 
-  let writer = std::fs::File::create(file)?;
-  let mut writer = BufWriter::new(writer);
-  compiler.writer_literals(&mut writer).unwrap();
-  compiler.write_insts(&mut writer).unwrap();
+  compiler.write_literals(writer).unwrap();
+  compiler.write_insts(writer).unwrap();
   println!(
-    "Written {} literals and {} instructions",
+    "Written {} literals and {} instructions to {out_file:?}",
     compiler.literals.len(),
     compiler.instructions.len()
   );
@@ -226,9 +250,7 @@ impl ByteCode {
     &mut self,
     reader: &mut impl Read,
   ) -> std::io::Result<()> {
-    let mut buf = [0; std::mem::size_of::<usize>()];
-    reader.read_exact(&mut buf)?;
-    let num_literals = usize::from_le_bytes(buf);
+    let num_literals = deserialize_size(reader)?;
     for _ in 0..num_literals {
       let mut buf = [0u8; std::mem::size_of::<i64>()];
       reader.read_exact(&mut buf)?;
@@ -241,9 +263,7 @@ impl ByteCode {
     &mut self,
     reader: &mut impl Read,
   ) -> std::io::Result<()> {
-    let mut buf = [0; std::mem::size_of::<usize>()];
-    reader.read_exact(&mut buf)?;
-    let num_instructions = usize::from_le_bytes(buf);
+    let num_instructions = deserialize_size(reader)?;
     for _ in 0..num_instructions {
       let inst = Instruction::deserialize(reader)?;
       self.instructions.push(inst);
@@ -254,7 +274,12 @@ impl ByteCode {
   fn interpret(&self) -> Option<f64> {
     let mut stack = vec![];
 
-    for instruction in &self.instructions {
+    for (ip, instruction) in
+      self.instructions.iter().enumerate()
+    {
+      dprintln!(
+        "interpret[{ip}]: {instruction:?} stack: {stack:?}"
+      );
       match instruction.op {
         OpCode::LoadLiteral => {
           stack.push(self.literals[instruction.arg0 as usize]);
@@ -276,28 +301,60 @@ impl ByteCode {
   }
 }
 
-fn read_program(file: &str) -> std::io::Result<ByteCode> {
-  let reader = std::fs::File::open(file)?;
-  let mut reader = BufReader::new(reader);
+fn read_program(
+  reader: &mut impl Read,
+) -> std::io::Result<ByteCode> {
   let mut bytecode = ByteCode::new();
-  bytecode.read_literals(&mut reader)?;
-  bytecode.read_instructions(&mut reader)?;
+  bytecode.read_literals(reader)?;
+  bytecode.read_instructions(reader)?;
   Ok(bytecode)
 }
 
-fn main() {
-  let mut args = std::env::args();
-  args.next();
-  match args.next().as_ref().map(|s| s as &str) {
-    Some("w") => write_program("bytecode.bin").unwrap(),
-    Some("r") => {
-      if let Ok(bytecode) = read_program("bytecode.bin") {
+fn main() -> std::io::Result<()> {
+  let Some(args) = parse_args() else { return Ok(()) };
+
+  match args.run_mode {
+    RunMode::Compile => {
+      if let Some(expr) = args.source {
+        let writer = std::fs::File::create(&args.output)?;
+        let mut writer = BufWriter::new(writer);
+        write_program(
+          &expr,
+          &mut writer,
+          &args.output,
+          args.disasm,
+        )?;
+      }
+    }
+    RunMode::Run(code_file) => {
+      let reader = std::fs::File::open(&code_file)?;
+      let mut reader = BufReader::new(reader);
+      match read_program(&mut reader) {
+        Ok(bytecode) => {
+          let result = bytecode.interpret();
+          println!("result: {result:?}");
+        }
+        Err(e) => eprintln!("Read program error: {e:?}"),
+      }
+    }
+    RunMode::CompileAndRun => {
+      if let Some(expr) = args.source {
+        let mut buf = vec![];
+        write_program(
+          &expr,
+          &mut std::io::Cursor::new(&mut buf),
+          "<Memory>",
+          args.disasm,
+        )?;
+        let bytecode =
+          read_program(&mut std::io::Cursor::new(&mut buf))?;
         let result = bytecode.interpret();
         println!("result: {result:?}");
       }
     }
-    _ => println!("Please specify w or r as an argument"),
+    _ => println!("Please specify -c or -r as an argument"),
   }
+  Ok(())
 }
 
 #[derive(Debug, PartialEq, Clone)]
